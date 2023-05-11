d078017b0348a472d4133ce7cc40ab0ecdc95ac9
Alex Bates <ajbates93@gmail.com>
2023-05-11 21:10:14 +0100

    feat: initial commit

---
+++ bates-thoughts/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <header class="p-5 bg-gray-100">Header</header>
+    <main class="min-h-[400px] p-5 text-left max-w-[1280px] mx-auto">
+      <div class="underline text-3xl">
+        What's up?
+      </div>
+    </main>
+    <footer class="p-5 bg-gray-100">Footer</footer>
+  </div>
+</template>
